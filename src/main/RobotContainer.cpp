f2662929bcf655f43a06868fd771dd6b765f8716
EricBrwn <a01572515@tec.mx>
2026-01-13 19:58:08 -0600

    New Repository :)

---
+++ src/main/RobotContainer.cpp
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "RobotContainer.h"
+
+#include <frc2/command/button/Trigger.h>
+
+RobotContainer::RobotContainer() {
+  // Initialize all of your commands and subsystems here
+
+  // Configure the button bindings
+  autoChooser = pathplanner::AutoBuilder::buildAutoChooser();
+	frc::SmartDashboard::PutData("AutoChooser", &autoChooser);
+
+  ConfigureBindings();
+
+}
+
+void RobotContainer::ConfigureBindings() {
+  // Configure your trigger bindings here
+
+  // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+
+  // Schedule `ExampleMethodCommand` when the Xbox controller's B button is
+  // pressed, cancelling on release.
+  ConfigDriverBindings();
+}
+
+void RobotContainer::ConfigDriverBindings() {
+    // Driver Controller Bindings
+
+    // Reset Heading
+}
+
+frc2::Command* RobotContainer::GetAutonomousCommand() {
+
+	return autoChooser.GetSelected();
+}
---
+++ src/main/RobotContainer.h
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/CommandPtr.h>
+#include <frc2/command/button/CommandXboxController.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <pathplanner/lib/auto/NamedCommands.h>
+#include <pathplanner/lib/auto/AutoBuilder.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include "OvertureLib/Gamepads/OverXboxController/OverXboxController.h"
+
+#include "Constants.h"
+
+/**
+ * This class is where the bulk of the robot should be declared.  Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls).  Instead, the structure of the robot (including subsystems,
+ * commands, and trigger mappings) should be declared here.
+ */
+class RobotContainer {
+ public:
+  RobotContainer();
+
+  frc2::Command* GetAutonomousCommand();
+
+ private:
+
+
+  // The robot's subsystems are defined here...
+	frc::SendableChooser<frc2::Command*> autoChooser;
+
+  void ConfigureBindings();
+  void ConfigDriverBindings();
+
+};
